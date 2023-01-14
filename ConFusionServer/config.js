c59dfb3e89e40d3120e857f0c93bb46060097bf9
Ashesh Pangma <81547202+AsheshPangma@users.noreply.github.com>
2023-01-14 11:18:46 +0100

    Add files via upload

---
+++ ConFusionServer/config.js
@@ -0,0 +1,8 @@
+module.exports = { 
+    'secretKey' : '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '482218496994312',
+        clientSecret :'cfae139a77a9f93a68da80ff12b57d9e'
+    }
+}
